1eaedfd2c473a5092d1b18685cc9788bc2dd7ba9
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-03-01 15:35:33 -0500

    switched to jpeg

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -3,23 +3,23 @@ const Projects = () => {
         <div className = 'Projects'>
             <article className= 'BookNRead'>
                 <h5>Book N Read</h5>
-                <img src = 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1632778817/Screen_Shot_2021-09-27_at_5.37.55_PM_epjm8z.png'/>
+                <img src = 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166749/Screen_Shot_2021-09-27_at_5.37.55_PM_epjm8z_Small_cdvxea.jpg'/>
             </article>
 
             <article className = 'Frizzy'>
                 <h5>Frizzy</h5>
-                <img src ='https://res.cloudinary.com/dx3a3l6k0/image/upload/v1632779116/Screen_Shot_2021-09-27_at_5.44.27_PM_cnvup1.png'/>
+                <img src ='https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166714/Screen_Shot_2021-09-27_at_5.44.27_PM_cnvup1_Small_ebjh13.jpg'/>
             </article>
         
             <article className= 'KDrama'>
                 <h5>K-Drama Love</h5>
-                <img src = 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1632780633/Screen_Shot_2021-09-27_at_6.09.45_PM_rmasxb.png'/>
+                <img src = 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166726/Screen_Shot_2021-09-27_at_6.09.45_PM_rmasxb_Small_ivofnq.jpg'/>
                 
             </article>
                 
             <article className= 'VideoGame'>
                 <h5>VideoGame</h5>
-                <img src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1643240979/Screen_Shot_2022-01-26_at_6.44.44_PM_slslke.png'/>
+                <img src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166738/Screen_Shot_2022-01-26_at_6.44.44_PM_slslke_Small_dr7vua.jpg'/>
             </article>
         
             
