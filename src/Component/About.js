64e25dfa0da81f23f4c003bfa8b5146e7d17a860
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-03-22 11:38:09 -0400

    about me change

--- src/Component/About.js
+++ src/Component/About.js
@@ -12,17 +12,17 @@ const About = () => {
             </Carousel> */}
             <img className= 'first'src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1641422294/6F754EF0-CFD7-47CD-A9DC-D4D968A7C229_1_102_o_crbx1p.jpg'/>
             <p className='about'>
-            Full-stack software engineer who has experience getting his hands messy; 
-            my unique background from working with executive chefs as well as surveying 
-            archeological digs has sharpened my interpersonal communication, 
-            honed in my attention to detail, and kept my passion for sifting 
-            through “the weeds” to realize the culmination of hard work alive and well. 
-            This background paired with my training has produced a software engineer who
-             enjoys the grit and grind of everyday coding, whether 
-             it’s investigating source codes for the backend, 
-             or styling up the front-end with a little flair (thanks plating!) 
-             I am hungry for new opportunities moving forward 
-            and am eager to learn more about your business and how I can help.
+            Goal-oriented and tech-savvy professional with expertise in 
+            software engineering and website development. Capable of designing 
+            network procedures, while ensuring compliance with latest upgrades and 
+            procedures. Astute at managing entire website and software development 
+            lifecycle from inception till completion within defined scope, budget,
+            and quality. Talent for devising and implementing software solutions, 
+            while automating and integrating systems to enhance productivity. 
+            Skilled in performing requirement gathering and rapid prototyping to 
+            reduce design and development time. Conversant communicator with 
+            exceptional interpersonal, relationship-building, 
+            problem-solving, and strategic / analytical thinking skills.
             </p>
         </div>
         
