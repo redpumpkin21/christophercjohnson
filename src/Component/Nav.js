5754d2606da1fd9564f94df9cc7fcbe211539367
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-03-22 11:56:12 -0400

    added linkedin site

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -9,9 +9,9 @@ const Nav = () => {
                     Home
                 </Link>
             
-            <a href= 'https://christophercjohnson.vercel.app/#form'>contact me</a>
-            
-            <a href='https://docs.google.com/document/d/1xV9EOuQh2WKUGFKEs3CxZUZSBPQIbJV3/edit?usp=sharing&ouid=107416650229481766405&rtpof=true&sd=true'> resume</a>
+            <a href= 'https://christophercjohnson.vercel.app/#form'>Contact Me</a>
+            <a href ='https://www.linkedin.com/in/christopher-code-johnson/'>Linkedin</a>
+            <a href='https://docs.google.com/document/d/1xV9EOuQh2WKUGFKEs3CxZUZSBPQIbJV3/edit?usp=sharing&ouid=107416650229481766405&rtpof=true&sd=true'> Resume</a>
         </div>
 
     )}
