9bad9473ef7f01e60ba03371d304b32a7f5fbce3
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-03-23 14:31:10 -0400

    open to new tab

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -9,9 +9,9 @@ const Nav = () => {
                     Home
                 </Link>
             
-            <a href= 'https://christophercjohnson.vercel.app/#form'>Contact Me</a>
-            <a href ='https://www.linkedin.com/in/christopher-code-johnson/'>Linkedin</a>
-            <a href='https://docs.google.com/document/d/1xV9EOuQh2WKUGFKEs3CxZUZSBPQIbJV3/edit?usp=sharing&ouid=107416650229481766405&rtpof=true&sd=true'> Resume</a>
+            <a href= 'https://christophercjohnson.vercel.app/#form' >Contact Me</a>
+            <a href ='https://www.linkedin.com/in/christopher-code-johnson/' target='_blank' rel= 'noreferror noopener'>Linkedin</a>
+            <a href='https://docs.google.com/document/d/1xV9EOuQh2WKUGFKEs3CxZUZSBPQIbJV3/edit?usp=sharing&ouid=107416650229481766405&rtpof=true&sd=true' target='_blank' rel= 'noreferror noopener'> Resume</a>
         </div>
 
     )}
