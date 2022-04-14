159a37cf21c4c4b3c724a91e96caecabb683e866
Christopher Johnson <86222389+redpumpkin21@users.noreply.github.com>
2022-04-14 16:13:12 -0400

    idk

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom"
 
 import {BsGithub, BsLinkedin, BsFillEnvelopeFill} from 'react-icons/bs'
-
+import {IoHomeSharp} from 'react-icons/io5'
 const Nav = () => {
     
     return (
         <div className= 'nav'>
             
             <Link to= '/'>
-                Home
+                <IoHomeSharp />
             </Link>            
             <a href= 'https://christophercjohnson.vercel.app/#form' ><BsFillEnvelopeFill /></a>
             <a href='https://www.linkedin.com/in/christopher-code-johnson/' target='_blank' rel= 'noreferror noopener'><BsLinkedin /></a>
