237f6acab1d689273c0720d69c977b6ddc1623c6
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-11-30 16:19:21 -0500

    updated projects

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Footer from './Component/Footer';
 import Home from './Pages/Home';
 import BookNRead from './Pages/BookNRead';
 import Frizzy from './Pages/Frizzy';
-import Kdrama from './Pages/Kdrama';
 import VideoGame from './Pages/VideoGame';
 import Todo from './Pages/Todo';
 
@@ -25,8 +24,7 @@ function App() {
           <Route path = '/about' element ={<About />}/>            
           <Route path ='/projects' element ={<Projects />}/> 
           <Route path = '/booknread' element ={<BookNRead/>}/>
-          <Route path = '/frizzy' element ={<Frizzy/>}/>
-          <Route path = '/K-dramalove' element ={<Kdrama/>}/>
+          <Route path = '/frizzy' element ={<Frizzy/>}/>          
           <Route path = '/videogame' element ={<VideoGame/>}/>
           <Route path= '/todo' element={<Todo />} />
         </Routes>        
--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -7,8 +7,16 @@ const Projects = () => {
             <article className= 'BookNRead'>
                 <h4>Book N Read</h4>
                 <img src = 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166749/Screen_Shot_2021-09-27_at_5.37.55_PM_epjm8z_Small_cdvxea.jpg'/>
-            
+                <div className= 'firstdescr'>
+                    <p>Desktop View</p>
+                    <p  className= 'bkdescr'>A website with a limited amount of videogames
+                        that you can see to show functionality of the site.
+                        search through and look at the games shown.
+                    </p>
                     <Link to= '/booknread'> Details</Link>
+                    
+                </div> 
+                    
                 
             </article>
 
@@ -16,8 +24,16 @@ const Projects = () => {
                 <h4>Frizzy</h4>
                 
                 <img src ='https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166714/Screen_Shot_2021-09-27_at_5.44.27_PM_cnvup1_Small_ebjh13.jpg'/>
-            
+                <div className= 'firstdescr'>
+                    <p>Desktop View</p>
+                    <p className= 'fzdescr'>A website with a limited amount of videogames
+                        that you can see to show functionality of the site.
+                        search through and look at the games shown.
+                    </p>
                     <Link to= '/frizzy'> Details</Link>
+                    
+                </div> 
+                    
                 
             </article>       
       
@@ -25,18 +41,35 @@ const Projects = () => {
             <article className= 'VideoGame'>
                 <h4>VideoGame</h4>
                 <img src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1646166738/Screen_Shot_2022-01-26_at_6.44.44_PM_slslke_Small_dr7vua.jpg'/>
-                    <p>
-                        Desktop view
-                        <Link to= '/videogame'> Details</Link>
+                <div className= 'firstdescr'>
+                    <p>Desktop View</p>
+                    <p className= 'vgdescr'>A website with a limited amount of videogames
+                        that you can see to show functionality of the site.
+                        search through and look at the games shown.
                     </p>
+                    <Link to= '/videogame'> Details</Link>
+                    
+                </div> 
+                    
                     
                       
             </article>
 
             <article className= 'Todo'>
                 <h4>Todo</h4>
-                <img src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/v1669837854/613BCE21-488F-4135-8753-C727FA1ED27A_odstrh.png'/>
+                <img src= 'https://res.cloudinary.com/dx3a3l6k0/image/upload/c_scale,w_300/v1669837998/613BCE21-488F-4135-8753-C727FA1ED27A_odstrh.png'/>
+                <div className= 'firstdescr'>
+                    <p>Mobile View</p>
+                    <p className= 'tddescr'>An application built mainly to use for mobile devices.
+                        Sitting on a chair on the computer can lead to a whole mess
+                        of procastinations. Using your phone limits it. So when using
+                        the app on your phone have realtime to do list where you can create
+                        what you need to get done and then click finish to be done with it forever.
+                    </p>
                     <Link to= '/todo'> Details</Link>
+                    
+                </div> 
+                    
             </article>
         
             
